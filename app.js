88236126a11a5e24ee8db9b50695f3037d1f8a87
rameshmani3133 <ramesh.mani.3133@gmail.com>
2026-09-09 13:51:43 +0530

    Remove completed voucher cleanup hook

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const { auditMiddleware } = require('./lib/audit');
 const { scheduleDailyBackup } = require('./lib/backup');
 const { scheduleDailyReminderEmails } = require('./lib/reminderService');
 const { ensureDatabaseSchema } = require('./lib/schemaSync');
-const { purgeAuthorizedVoucherPair } = require('./lib/oneTimeMaintenance');
 require('dotenv').config();
 
 const app = express();
@@ -88,7 +87,6 @@ app.get('*', (req, res) => {
 // Start Server
 async function start() {
     await ensureDatabaseSchema(prisma);
-    await purgeAuthorizedVoucherPair(prisma);
     await ensureSuperAdmin();
     scheduleDailyBackup(prisma);
     scheduleDailyReminderEmails(prisma);
